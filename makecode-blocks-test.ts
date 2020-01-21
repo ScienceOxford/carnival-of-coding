ecfab1178bf60c23d2742464f2ae3def9087a738
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2020-01-21 16:39:16 +0000

    worked code

--- makecode-blocks-test.ts
+++ makecode-blocks-test.ts
@@ -4,17 +4,23 @@ input.onButtonPressed(Button.A, function () {
     carnivalOfCoding.turn(LED.red, 1)
     carnivalOfCoding.turn(LED.amber, 1)
     carnivalOfCoding.turn(LED.green, 1)
-    carnivalOfCoding.motor(Motor.A, -300, 1000)
-    carnivalOfCoding.motor(Motor.B, -300, 1000)
+    carnivalOfCoding.motorAdvanced(Motor.A, -300, 1000)
+    carnivalOfCoding.motorAdvanced(Motor.B, -300, 1000)
     basic.showIcon(IconNames.Heart)
     carnivalOfCoding.turn(LED.red, 0)
     carnivalOfCoding.turn(LED.amber, 0)
     carnivalOfCoding.turn(LED.green, 0)
     basic.pause(200)
-    carnivalOfCoding.motor(Motor.AB, 300, 1000)
+    carnivalOfCoding.motorAdvanced(Motor.AB, 300, 1000)
 })
 
 input.onButtonPressed(Button.B, function () {
+    carnivalOfCoding.motor(
+    Motor.A,
+    Dir.forward,
+    Speed.slow,
+    2000
+    )
     carnivalOfCoding.outputDigital(1)
     basic.pause(1000)
     carnivalOfCoding.outputDigital(0)
